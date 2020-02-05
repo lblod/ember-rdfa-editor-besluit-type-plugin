893b8878c8d1d81ed51afc8204e6a2f5bc9680ef
Oscar <lagartoverde97@gmail.com>
2020-02-05 11:10:08 +0100

    First commit

---
+++ app/components/editor-plugins/besluit-type-card.js
@@ -0,0 +1,1 @@
+export { default } from '@lblod/ember-rdfa-editor-besluit-type-plugin/components/editor-plugins/besluit-type-card';
---
+++ app/services/rdfa-editor-besluit-type-plugin.js
@@ -0,0 +1,1 @@
+export { default } from '@lblod/ember-rdfa-editor-besluit-type-plugin/services/rdfa-editor-besluit-type-plugin';
