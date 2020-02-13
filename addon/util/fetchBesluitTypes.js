c82cd80239a6faedfa1292e3be0f7da0ad62a4df
Oscar <lagartoverde97@gmail.com>
2020-02-13 10:40:03 +0100

    added semicolons to fetchBesluitTypes file

--- addon/util/fetchBesluitTypes.js
+++ addon/util/fetchBesluitTypes.js
@@ -2,23 +2,23 @@ import rdflib from 'ember-rdflib';
 
 export default async function fetchBesluitTypes() {
   const besluitTypesGraph = new rdflib.NamedNode("http://data.lblod.info/besluitTypes");
-  const response = await fetch('/assets/ttl/20200120153300-insert-besluit-types.ttl')
-  const text = await response.text()
-  const graph = rdflib.graph()
+  const response = await fetch('/assets/ttl/20200120153300-insert-besluit-types.ttl');
+  const text = await response.text();
+  const graph = rdflib.graph();
   await rdflib.parse(text, graph, besluitTypesGraph.value, "text/turtle");
   const type = new rdflib.NamedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type');
   const conceptPredicate = new rdflib.NamedNode('http://www.w3.org/2004/02/skos/core#Concept');
-  const match = graph.match(undefined, type, conceptPredicate)
+  const match = graph.match(undefined, type, conceptPredicate);
   const besluitTypes = match.map((typeNode) => {
-    const uri = typeNode.subject.value
-    const uuid = uri.split('/').pop()
+    const uri = typeNode.subject.value;
+    const uuid = uri.split('/').pop();
     const uriNode = new rdflib.NamedNode(uri);
     const prefLabelNode = new rdflib.NamedNode('http://www.w3.org/2004/02/skos/core#prefLabel');
-    const prefLabelMatch = graph.match(uriNode, prefLabelNode)
+    const prefLabelMatch = graph.match(uriNode, prefLabelNode);
     return {
       typeAttribute: `besluittype:${uuid}`,
       label: prefLabelMatch[0].object.value
-    }
-  })
-  return besluitTypes
+    };
+  });
+  return besluitTypes;
 }
