f8c899989740091118cf75f6eb4bf1d0349a3cf1
Claire Lovisa <lovisa.claire@gmail.com>
2020-02-17 10:51:20 +0100

    Upgrade to Ember v3.15

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
+    ecmaFeatures: {
+      legacyDecorators: true
+    }
   },
   plugins: [
     'ember'
@@ -15,7 +19,8 @@ module.exports = {
     browser: true
   },
   rules: {
-    semi: 'warn'
+    semi: 'warn',
+    'ember/no-jquery': 'error'
   },
   overrides: [
     // node files
--- README.md
+++ README.md
@@ -7,7 +7,7 @@ RDFa editor plugin to change the type of a besluit
 Compatibility
 ------------------------------------------------------------------------------
 
-* Ember.js v3.4 or above
+* Ember.js v3.8 or above
 * Ember CLI v2.13 or above
 * Node.js v8 or above
 
