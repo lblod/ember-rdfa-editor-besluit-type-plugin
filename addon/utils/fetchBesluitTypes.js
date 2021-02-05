510740eea4833f86b2d58ead876377acb2d0e38b
Oscar <lagartoverde97@gmail.com>
2021-02-05 11:21:41 +0100

    changed the file we fetch from the frontend

--- addon/utils/fetchBesluitTypes.js
+++ addon/utils/fetchBesluitTypes.js
@@ -2,7 +2,7 @@ import rdflib from 'ember-rdflib';
 
 export default async function fetchBesluitTypes() {
   const besluitTypesGraph = new rdflib.NamedNode("http://data.lblod.info/besluitTypes");
-  const response = await fetch('/assets/ttl/20200120153300-insert-besluit-types.ttl');
+  const response = await fetch('/assets/ttl/20210205102900-new-besluit-types.ttl');
   const text = await response.text();
   const graph = rdflib.graph();
   await rdflib.parse(text, graph, besluitTypesGraph.value, "text/turtle");
