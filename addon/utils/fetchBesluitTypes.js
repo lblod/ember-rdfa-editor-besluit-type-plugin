9c47187ac8f0e8f63c3218813ddffe1c0c833881
abeforgit <arnebertrand@gmail.com>
2021-04-09 20:58:06 +0200

    Fix spacing

--- addon/utils/fetchBesluitTypes.js
+++ addon/utils/fetchBesluitTypes.js
@@ -17,17 +17,17 @@ export default async function fetchBesluitTypes(classificatieUri) {
     const decidableByNode = new rdflib.NamedNode('http://lblod.data.gift/vocabularies/besluit/decidableBy');
     const decidableByMatch = graph.match(uriNode, decidableByNode);
     let valid = false;
-    for(const match of decidableByMatch) {
-      if(match.object.value === classificatieUri) {
+    for (const match of decidableByMatch) {
+      if (match.object.value === classificatieUri) {
         valid = true;
       }
     }
-    if(!valid) return;
+    if (!valid) return;
 
     const prefLabelNode = new rdflib.NamedNode('http://www.w3.org/2004/02/skos/core#prefLabel');
     const prefLabelMatch = graph.match(uriNode, prefLabelNode);
-    const subTypes=getSubTypes(graph, uriNode);
-    const result={
+    const subTypes = getSubTypes(graph, uriNode);
+    const result = {
       typeAttribute: `besluittype:${uuid}`,
       label: prefLabelMatch[0].object.value,
       subTypes: subTypes
@@ -36,24 +36,25 @@ export default async function fetchBesluitTypes(classificatieUri) {
   }).filter(type => type);
   return besluitTypes;
 }
-function getSubTypes(graph, uriNode){
+
+function getSubTypes(graph, uriNode) {
   const prefLabelNode = new rdflib.NamedNode('http://www.w3.org/2004/02/skos/core#prefLabel');
-  const broader=new rdflib.NamedNode('http://www.w3.org/2004/02/skos/core#broader');
+  const broader = new rdflib.NamedNode('http://www.w3.org/2004/02/skos/core#broader');
 
-  const subTypesUris=graph.match(undefined, broader, uriNode);
-  const subTypeLabels=subTypesUris.map(triple=>{
-    const uri=new rdflib.NamedNode(triple.subject.value);
+  const subTypesUris = graph.match(undefined, broader, uriNode);
+  const subTypeLabels = subTypesUris.map(triple => {
+    const uri = new rdflib.NamedNode(triple.subject.value);
     const uuid = triple.subject.value.split('/').pop();
-    const subTypeTriple=graph.match(uri, prefLabelNode, undefined);
+    const subTypeTriple = graph.match(uri, prefLabelNode, undefined);
 
-    const result={
+    const result = {
       typeAttribute: `besluittype:${uuid}`,
       label: subTypeTriple[0].object.value,
       subTypes: []
-    }
+    };
     //recursive logic for sub-sub-types
-    if(getSubTypes(graph, uri).length!=0){
-      result.subTypes=getSubTypes(graph, uri);
+    if (getSubTypes(graph, uri).length != 0) {
+      result.subTypes = getSubTypes(graph, uri);
     }
     return result;
   });
