2990bc1eb922a252896b356c5a45bf090c78f423
“[BenJay10]” <[benbehaeghe@gmail.com]>
2022-07-05 13:55:33 +0200

    Config is now configurable with Docker variables
    Using Docker environment variables, you can now configure the URL to the
    SPARQL endpoint (for Centrale Vindplaats) where you can find the
    BesluitTypes.

--- config/environment.js
+++ config/environment.js
@@ -3,7 +3,7 @@
 module.exports = function (/* environment, appConfig */) {
   return {
     'besluit-type-plugin': {
-      'besluit-types-endpoint': 'http://localhost:8895/sparql',
+      'besluit-types-endpoint': '{{BESLUIT_TYPES_SPARQL_ENDPOINT}}',
     },
   };
 };
