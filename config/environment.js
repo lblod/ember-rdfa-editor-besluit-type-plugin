54bbce7d74d4cba7328d23bda6f19b5dd7bd16d7
benjay10 <ben.behaeghe@redpencil.io>
2022-07-14 23:13:02 +0200

    Reset the config to the correct value
    While testing, accidentally pushed changes to this file.

--- config/environment.js
+++ config/environment.js
@@ -3,9 +3,7 @@
 module.exports = function (/* environment, appConfig */) {
   return {
     'besluit-type-plugin': {
-      //'besluit-types-endpoint': '{{BESLUIT_TYPES_SPARQL_ENDPOINT}}',
-      'besluit-types-endpoint': 'https://centrale-vindplaats.lblod.info/sparql',
-      //'besluit-types-endpoint': 'http://localhost:83/sparql',
+      'besluit-types-endpoint': '{{BESLUIT_TYPES_SPARQL_ENDPOINT}}',
     },
   };
 };
