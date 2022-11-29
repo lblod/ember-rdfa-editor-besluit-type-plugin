57575e3ee41bd8aab72d5c9ecfbd6155738ce1ad
Elena Poelman <elena.poelman@redpencil.io>
2022-11-29 14:32:07 +0100

    convert plugin to prosemirror based editor plugin

--- addon/besluit-type-plugin.js
+++ addon/besluit-type-plugin.js
@@ -1,3 +1,5 @@
+import { RdfaEditorPlugin } from '@lblod/ember-rdfa-editor';
+
 /**
  * Entry point for BesluitTypePlugin
  *
@@ -6,33 +8,46 @@
  * @constructor
  * @extends EmberService
  */
-export default class BesluitTypePlugin {
-  /**
-   * Handles the incoming events from the editor dispatcher.  Responsible for generating hint cards.
-   *
-   * @method execute
-   *
-   * @param {string} hrId Unique identifier of the state in the HintsRegistry.  Allows the
-   * HintsRegistry to update absolute selected regions based on what a user has entered in between.
-   * @param {Array} rdfaBlocks Set of logical blobs of content which may have changed.  Each blob is
-   * either has a different semantic meaning, or is logically separated (eg: a separate list item).
-   * @param {Object} hintsRegistry Keeps track of where hints are positioned in the editor.
-   * @param {Object} editor Your public interface through which you can alter the document.
-   *
-   * @public
-   */
-  controller;
+// export default class BesluitTypePlugin {
+//   /**
+//    * Handles the incoming events from the editor dispatcher.  Responsible for generating hint cards.
+//    *
+//    * @method execute
+//    *
+//    * @param {string} hrId Unique identifier of the state in the HintsRegistry.  Allows the
+//    * HintsRegistry to update absolute selected regions based on what a user has entered in between.
+//    * @param {Array} rdfaBlocks Set of logical blobs of content which may have changed.  Each blob is
+//    * either has a different semantic meaning, or is logically separated (eg: a separate list item).
+//    * @param {Object} hintsRegistry Keeps track of where hints are positioned in the editor.
+//    * @param {Object} editor Your public interface through which you can alter the document.
+//    *
+//    * @public
+//    */
+//   controller;
 
-  get name() {
-    return 'besluit-type';
-  }
+//   get name() {
+//     return 'besluit-type';
+//   }
 
-  initialize(controller) {
-    this.controller = controller;
-    controller.registerWidget({
-      componentName: 'editor-plugins/toolbar-dropdown',
-      identifier: 'besluit-type-plugin/dropdown',
-      desiredLocation: 'toolbar',
-    });
+//   initialize(controller) {
+//     this.controller = controller;
+//     controller.registerWidget({
+//       componentName: 'editor-plugins/toolbar-dropdown',
+//       identifier: 'besluit-type-plugin/dropdown',
+//       desiredLocation: 'toolbar',
+//     });
+//   }
+// }
+
+export default class BesluitTypePlugin extends RdfaEditorPlugin {
+
+  widgets(){
+    return [
+      {
+        componentName: 'editor-plugins/toolbar-dropdown',
+        identifier: 'besluit-type-plugin/dropdown',
+        desiredLocation: 'toolbarRight',
+      }
+    ]
   }
 }
